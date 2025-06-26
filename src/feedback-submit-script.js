39d4ac3cc2f36b338a922355cdd9ca878f78a6ca
RPope-TechAdmin <rpope@purenv.au>
2025-06-26 10:59:19 +1000

    Manipulating the secret code accessibility and removing file type forcing

--- src/feedback-submit-script.js
+++ src/feedback-submit-script.js
@@ -7,9 +7,9 @@ document.getElementById("dataForm").addEventListener("submit", async (e) => {
     feedback: formData.get("feedback")
   };
 
-  const res = await fetch("https://https://calm-smoke-0485c311e.2.azurestaticapps.net/feedback", {
+  const res = await fetch("https://https://calm-smoke-0485c311e.2.azurestaticapps.net/feedback?code=${ secrets.BACKEND_API_KEY_DEFAULT }", {
     method: "POST",
-    headers: { "Content-Type": "application/json" },
+    headers: { "Accept": "application/json" },
     body: JSON.stringify(payload)
   });
 
